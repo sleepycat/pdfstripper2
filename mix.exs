76186c44b9efbdd187241788fca7658d3da72c26
Mike Williamson <mike@korora.ca>
2015-07-10 14:48:09 -0400

    Bump version to 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pdfstripper2.Mixfile do
   def project do
     [
       app: :pdfstripper2,
-      version: "0.0.2",
+      version: "1.0.0",
       elixir: "~> 1.0",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix] ++ Mix.compilers,
